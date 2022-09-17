c32097d54bf4e87048aec77b22c9702e5f8846cc
DynamicDaikon <hietasoba@gmail.com>
2022-09-17 16:04:26 +0900

    add loading text

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import { getAllPokemon } from './utils/pokemon';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
       // get all data of Pokemon
       let res = await getAllPokemon(initialURL);
       console.log(res);
+      setLoading(false);
     };
 
     fetchPokemonData();
   }, []);
 
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      {loading ? <h1>Now Loading....</h1> : <h1>Load Completed</h1>}
+    </div>
+  );
 }
 
 export default App;
