d1a38bd126f1936566652a4e686df96e33a3af01
DynamicDaikon <hietasoba@gmail.com>
2022-12-28 22:27:39 +0900

    Revert "save wip"
    This reverts commit 570ab4400087dac979db89f8b57a3d67a74109ad.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { getAllPokemon, getPokemon } from './utils/pokemon';
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
   const [loading, setLoading] = useState(true);
-  const [pokemonData, setPokemonData] = useState([]);
 
   useEffect(() => {
     const fetchPokemonData = async () => {
@@ -24,12 +23,9 @@ function App() {
       data.map((pokemon) => {
         // console.log(pokemon);
         let pokemonRecord = getPokemon(pokemon.url);
-
         return pokemonRecord;
       }),
     );
-    setPokemonData(_pokemonData);
-    console.log(_pokemonData);
   };
 
   return (
--- src/utils/pokemon.js
+++ src/utils/pokemon.js
@@ -4,8 +4,4 @@ export const getAllPokemon = (url) => {
   });
 };
 
-export const getPokemon = (url) => {
-  return new Promise((resolve, reject) => {
-    fetch(url).then((res) => res.json().then((data) => resolve(data)));
-  });
-};
+export const getPokemon = () => {};
