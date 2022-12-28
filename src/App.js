59d1d2c7eaadbb82b3793e20b3df549f9931637d
DynamicDaikon <hietasoba@gmail.com>
2022-12-28 22:32:58 +0900

    for conflict

--- src/App.js
+++ src/App.js
@@ -2,7 +2,35 @@ import './App.css';
 
 function App() {
   const initialURL = 'https://pokeapi.co/api/v2/pokemon';
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchPokemonData = async () => {
+      // get all data of Pokemon
+      let res = await getAllPokemon(initialURL);
+      // 各ポケモンの詳細なデータを取得
+      loadPokemon(res.results);
+      setLoading(false);
+    };
+
+    fetchPokemonData();
+  }, []);
+
+  const loadPokemon = (data) => {
+    let _pokemonData = Promise.all(
+      data.map((pokemon) => {
+        // console.log(pokemon);
+        let pokemonRecord = getPokemon(pokemon.url);
+        return pokemonRecord;
+      }),
+    );
+  };
+
+  return (
+    <div className="App">
+      {loading ? <h1>ロード中</h1> : <h1>ポケモンデータを取得しました。</h1>}
+    </div>
+  );
 }
 
 export default App;
